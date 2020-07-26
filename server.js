7d60ee2e349bdc9d943ad275a736a51640d22c00
Diego Cordoba <diegocordoba87@hotmail.com>
2020-07-26 19:43:49 -0400

    added htmlroutes and tested them

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require ("mongoose");
+const path = require("path")
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
+//const db = require("./models");
 
 const app = express();
 
-app.use(logger("dev"));
+//app.use(logger("dev"));
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/workouts", { useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/workouts", { useNewUrlParser: true, useUnifiedTopology: true});
 
-
-require("./seeders/seed")
-require("routes/apiRoutes.js");
-require("routes/htmlRoutes");
+// app.get("/exercise", (req, res) =>{
+//   res.sendFile(path.join(__dirname, "./public/exercise.html"))
+// })
+//require("./seeders/seed")
+//require("routes/apiRoutes.js");
+require("./routes/htmlRoutes.js")(app);
 
 
 
