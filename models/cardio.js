0f80141a33018aef86e925dea8fa5bc62396102f
Diego Cordoba <diegocordoba87@hotmail.com>
2020-07-26 10:30:22 -0400

    added models folder, added cardio model and server.js but have not finished the server.js

---
+++ models/cardio.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CardioSchema = new Schema({
+    name: String,
+    duration: Number
+})
+
+const Cardio = mongoose.model("Cardio", CardioSchema);
+
+module.exports = Cardio;
