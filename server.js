e9d4e11c732e07c16ef5c8f93bcc41e41d89ed92
Diego Cordoba <diegocordoba87@hotmail.com>
2020-07-27 17:22:54 -0400

    removed mongoose connect from server since seeds already has it

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/workouts", { useNewUrlParser: true, useUnifiedTopology: true});
+
 
 require("./seeders/seed");
 require("./routes/apiRoutes.js")(app);
