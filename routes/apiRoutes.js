b2e5b9fc35eb1d9f6ef7f1b766b73c49ee83eee1
Diego Cordoba <diegocordoba87@hotmail.com>
2020-07-26 20:10:54 -0400

    added apit/workout route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,11 @@
+const { db } = require("../models/workout")
 
 
 module.exports = (app)=>{
     app.get("api/workouts", (req, res)=>{
-       
+       db.Workout.find().then((results)=>{
+           res.json(results);
+       })
     })
 
 
