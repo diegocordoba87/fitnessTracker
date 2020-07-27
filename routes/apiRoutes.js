9bd33aeb8d8279c65552d18af1799b841663d784
Diego Cordoba <diegocordoba87@hotmail.com>
2020-07-26 20:23:28 -0400

    added post and out routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,7 +8,25 @@ module.exports = (app)=>{
        })
     })
 
+    app.get("/api/workouts/range", (req, res)=>{
+        db.Workout.find().then(response => {
+      return res.json(response);
+    })
+    })
 
+    app.post("/api/workout", (req, res)=>{
+        db.Workout.create(req.body).then((response)=>{
+            return res.json(response);
+        })
+    })
+
+    app.put("/apit/workout/:id", (req, res)=>{
+        db.Workout.update({_id: req.params.id}, {$push:{exercises:req.body}}).then((response)=>{
+            return res.json(response)
+        })
+    })
+    
+    
 
 
 
